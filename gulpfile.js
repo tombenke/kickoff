efa29238e5ac22a86692c7bd740b5eec41fc9845
Tamás Benke <tombenke@gmail.com>
2014-11-20 10:28:49 +0100

    Add markdown and mustache modules to docgen task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
-var prompt = require('gulp-prompt');
+var mustache = require('gulp-mustache');
+var rename = require('gulp-rename');
+var through = require('gulp-through');
+var debug = require('gulp-debug');
+var order = require('gulp-order');
+var buffer = require('vinyl-buffer');
+var markdown = require('gulp-markdown');
 
-gulp.task('default', function() {
-    gulp.src('README.md')
-        .pipe(prompt.prompt([{
-            type: 'input',
-            name: 'appname',
-            message: 'The name of the application:',
-            default: 'anonymous'}], function(res) {
-                console.log('questionnaire: ', res);
-            }));
+gulp.task('docgen', function() {
+    gulp.src('docs/*.md', { base: process.cwd() })
+        .pipe(buffer())
+        .pipe(markdown())
+        .pipe(rename(function(path) {
+            path.extname = '.html';
+        }))
+        .pipe(through('subs', function(file, config) {
+            var content = file.contents.toString();
+            console.log('================================');
+            console.log('subs.content: ', content);
+            gulp.src('docs/pageTemplate.html')
+                .pipe(buffer())
+                .pipe(mustache({body: content}))
+                .pipe(debug())
+                .pipe(rename(file.path))
+                .pipe(gulp.dest());
+        }, {})());
 });
