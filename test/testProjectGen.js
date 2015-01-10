875adb9d1c0a5e342aaf1b6bc8471fcdafe60400
Tamás Benke <tombenke@gmail.com>
2015-01-10 18:37:34 +0100

    Update the test checksum

--- test/testProjectGen.js
+++ test/testProjectGen.js
@@ -11,7 +11,8 @@ var getChecksum = function(ctx, cb) {
         if (err) throw err;
         ctx.result1 = hashes;
         ctx.result2 = checksum(JSON.stringify(hashes, null, 2));
-        // console.log(ctx.result1, ctx.result2);
+        console.log('expected1: %s', JSON.stringify(ctx.result1, null, '    '));
+        console.log('expected2: "%s"', ctx.result2);
         cb(null, ctx);
     });
 };
@@ -43,7 +44,15 @@ describe('Generates a new project from archetype', function() {
             expected1: {
                 "files": {
                     ".gitignore": "9193e35d8b7fc33d768461505160c12c96c608bd",
-                    'ReadMe.md': 'f8c8bec34cc115169049f664fafaf7e26e109843',
+                    "LICENCE.md": "1ce3fa9b882c13a5aa2575b98f5a379f6a00a8bc",
+                    "ReadMe.md": "f8c8bec34cc115169049f664fafaf7e26e109843",
+                    "config.json": "9e0cc0cf16dddd314145fd1579426b7f78fe3f93",
+                    "data": {
+                        "files": {
+                            "data.json": "60cce35e64b84ac3ee19a2c386d0268857e26292"
+                        },
+                        "hash": "ed13eb4043a8646d7a323e8259cbc0e6448defb8"
+                    },
                     "docs": {
                         "files": {
                             "new_app-page-template.html": "c7aa9952c44e228e3144e03d256251eaff01d864"
@@ -51,9 +60,9 @@ describe('Generates a new project from archetype', function() {
                         "hash": "e7760370dfb44b3b5379f5f32cad4b7c5d98550f"
                     }
                 },
-                "hash": "7d837eb1a5ba16c089d2f0d551267abf7f83656c"
-            },
-            expected2: "25ff213eac36f663c23a7a924246c7fae6beab94"
+                "hash": "7711f2c6432cc82d5d8b80ab5d5a9d27ad28dcf8"
+            }, 
+            expected2:  "fea38eaaadb8c44bfac44e53f9a29de8d97f0050"
         };
 
         async.waterfall([
